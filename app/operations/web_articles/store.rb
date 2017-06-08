d669755b8d29f118ab288fcb0ac2b95b6030f3a6
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-06-08 13:44:54 +0200

    store content

--- app/operations/web_articles/store.rb
+++ app/operations/web_articles/store.rb
@@ -1,7 +1,12 @@
 module WebArticles
   class Store
-    def call
+    pattr_initialize :article_data
 
+    def call
+      web_article = WebArticle.new
+      web_article.content = article_data.fetch("content")
+      web_article.save
+      web_article
     end
   end
 end
--- spec/operations/web_articles/store_spec.rb
+++ spec/operations/web_articles/store_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe WebArticles::Store do
-
+  let(:article_data) do
+    {
+      "title" => "Sample title",
+      "content" => "Sample content",
+      "author" => "John Kowalski",
+      "date_published" => "John Kowalski",
+      "url" => "John Kowalski",
+      "word_count" => "John Kowalski",
+      "image_urls" => [
+        "http://www.image.com/1.jpg", 
+        "http://www.image.com/2.jpg", 
+        "http://www.image.com/3.jpg",
+      ],
+    }
+  end
+  let(:book) { create(:book)}
+  subject { described_class.new(article_data).call }
+  it "stores article content" do
+    expect(subject.content).to eq("Sample content")
+  end
 end
