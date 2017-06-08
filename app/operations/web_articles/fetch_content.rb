a34cd1f3d58ed2577dc0eb66ff370bacc36b46e9
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-06-08 12:48:29 +0200

    move implementation from test to class FetchContent

--- app/operations/web_articles/fetch_content.rb
+++ app/operations/web_articles/fetch_content.rb
@@ -2,11 +2,7 @@ require 'rest-client'
 
 module WebArticles
   class FetchContent
-    attr_reader :article_url
-    
-    def initialize(article_url)
-      @article_url = article_url
-    end
+    pattr_initialize :article_url
 
     def call
       api_url = "https://mercury.postlight.com/parser"
@@ -23,7 +19,9 @@ module WebArticles
         }
       )
       article_data = JSON.parse(response)
-      article_data
+      doc = Nokogiri::HTML(article_data["content"])
+      urls = doc.css("img").map { |img_tag| img_tag.attributes["src"].value }
+      article_data.merge({"image_urls" => urls})
     end
   end
 end
