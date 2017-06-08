4ccd8f3b3d5081ccfe85d877e0d82dc215c41f18
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-06-08 13:01:22 +0200

    refactor

--- app/operations/web_articles/fetch_content.rb
+++ app/operations/web_articles/fetch_content.rb
@@ -2,38 +2,43 @@ require 'rest-client'
 
 module WebArticles
   class FetchContent
+    API_KEY = Rails.application.config.mercury_postlight_api_key
+    API_URL = Rails.application.config.mercury_postlight_api_url
     pattr_initialize :article_url
 
     def call
-      response = execute_request
-      article_data = JSON.parse(response)
-      doc = Nokogiri::HTML(article_data["content"])
-      urls = doc.css("img").map { |img_tag| img_tag.attributes["src"].value }
-      article_data.merge({"image_urls" => urls})
+      article_data_with_images_urls
     end
 
     private
 
-    def execute_request
+    def article_data_with_images_urls
+      article_data.merge(
+        { "image_urls" => images_urls(article_data) }
+      )
+    end
+
+    def article_data
+      @article_data ||=  JSON.parse(execute_request_to_api)
+    end
+
+    def images_urls(article_data)
+      doc = Nokogiri::HTML(article_data["content"])
+      doc.css("img").map { |img_tag| img_tag.attributes["src"].value }
+    end
+
+    def execute_request_to_api
       RestClient::Request.execute(
         method: :get, 
-        url: api_url, 
+        url: API_URL, 
         headers: {
           "Content-Type" => "application/json", 
-          "x-api-key" => api_key,
+          "x-api-key" => API_KEY,
           "params" => {
             "url" => article_url
           },
         }
       )
-    end
-
-    def api_url
-      Rails.application.config.mercury_postlight_api_url
-    end
-
-    def api_key
-      Rails.application.config.mercury_postlight_api_key
     end
   end
 end
