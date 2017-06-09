d634b7c1fdca3c3c0a14d996cc942ed2cf767e97
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-06-09 09:08:09 +0200

    fetch articles for all url

--- app/operations/ebooks/create.rb
+++ app/operations/ebooks/create.rb
@@ -4,6 +4,9 @@ module Ebooks
 
     def call
       Book.create(name: book_name)
+      articles_data = urls.map do |url|
+        WebArticles::FetchContent.new(url).call
+      end
     end
   end
 end
