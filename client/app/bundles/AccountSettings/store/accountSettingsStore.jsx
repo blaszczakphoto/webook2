d20db920f5c4081eaf31d47a4752c3a39fe8d046
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-07-14 12:17:49 +0300

    add redux logger

--- client/app/bundles/AccountSettings/store/accountSettingsStore.jsx
+++ client/app/bundles/AccountSettings/store/accountSettingsStore.jsx
@@ -1,8 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import accountSettingsReducer from '../reducers/accountSettingsReducer';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+const middlewares = [thunk];
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
 
 const configureStore = (railsProps) => (
-  createStore(accountSettingsReducer, railsProps)
+  createStore(accountSettingsReducer, railsProps, applyMiddleware(...middlewares))
 );
 
 export default configureStore;
