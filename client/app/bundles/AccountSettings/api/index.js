26d8a0feb75c1a8f2e7953fc30c2a7f5af2e2fd9
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-07-15 23:32:38 +0300

    refactor api.js. step 2

--- client/app/bundles/AccountSettings/api/index.js
+++ client/app/bundles/AccountSettings/api/index.js
@@ -1,15 +1,15 @@
 import ReactOnRails from 'react-on-rails';
 
 export const updateKindleEmail = (kindleEmail) => {
-  const header = {
+  return fetch('/account_settings/update', {
     headers: ReactOnRails.authenticityHeaders({
       'Accept': 'application/json',
       'Content-Type': 'application/json',
     }),
     method: 'POST',
-    body: JSON.stringify({ kindle_email: kindleEmail }),
+    body: JSON.stringify({ 
+      kindle_email: kindleEmail 
+    }),
     credentials: 'same-origin',
-  };
-  console.log(header);
-  return fetch('/account_settings/update', header);
+  });
 }
