f0ac3e5527732b710b80cfaaac29eb89c06ae94f
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-07-17 12:39:13 +0300

    add valid context to spec

--- spec/operations/web_articles/image_validator_spec.rb
+++ spec/operations/web_articles/image_validator_spec.rb
@@ -3,12 +3,22 @@ require 'rails_helper'
 RSpec.describe WebArticles::ImageValidator do
   subject { described_class.new(url).call }
 
+  context "url is valid" do
+    let(:url) do
+      "http://www.seerinteractive.com/wp-content/uploads/2011/11/seer-icon_rocket-300x300.png"
+    end
+
+    it "returns the same passed url" do
+      expect(subject).to eq(url)
+    end
+  end
+
   context "url is weird" do
     let(:url) do
       "http://www.seerinteractive.com/wp-content/uploads/2011/11/seer-icon_rocket-300x300.png%20300w,%20http://www.seerinteractive.com/wp-content/uploads/2011/11/seer-icon_rocket-150x150.png%20150w,%20http://www.seerinteractive.com/wp-content/uploads/2011/11/seer-icon_rocket-80x80.png%2080w,%20http://www.seerinteractive.com/wp-content/uploads/2011/11/seer-icon_rocket-60x60.png%2060w,%20http://www.seerinteractive.com/wp-content/uploads/2011/11/seer-icon_rocket.png%20500w"
     end
 
-    it "returns valid url" do
+    it "returns first valid url from the string" do
       expect(subject).to eq("http://www.seerinteractive.com/wp-content/uploads/2011/11/seer-icon_rocket-300x300.png")
     end
   end
