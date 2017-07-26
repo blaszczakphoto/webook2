655c3339942acf7a748d462aa6d2236acfd94c58
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-07-26 16:39:44 +0200

    add test for controller send to kindle. next step

--- spec/controllers/send_to_kindle_controller_spec.rb
+++ spec/controllers/send_to_kindle_controller_spec.rb
@@ -8,10 +8,17 @@ RSpec.describe SendToKindleController, type: :controller do
     let(:params) do 
       { params: { book_id: 2 } } 
     end
+
+    subject { get :send_email, params }
+
     it "initialize operation with valid params" do
       get :send_email, params
       send_operation_object = double("SendToKindle::Send")
       expect(SendToKindle::Send).to receive(:new).with(kindle_email: "john.doe@kindle.com", book_id: "2")
+    end
+
+    it "redirect to webooks index" do
+      expect(subject).to redirect_to(webooks_index_path)
     end
   end
 end
