a9bf04cd7bb7009b2a5b807df3a3578afbbb83f3
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-07-26 14:23:07 +0200

    add spec for mail. finish

--- spec/mailers/send_to_kindle_mailer_spec.rb
+++ spec/mailers/send_to_kindle_mailer_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe SendToKindleMailer, type: :mailer do
   it "attach valid recipient" do
     expect(subject.to).to eq(["mariusz.blaszczak@gmail.com"])
   end
+
+  it "attach valid sender" do
+    expect(subject.from).to eq(["webook075@gmail.com"])
+  end
+
+  it "sends email" do
+    expect { subject.deliver_now }.to change { ActionMailer::Base.deliveries.count }.by(1)
+  end
+
+  it "attach mobi file" do
+    expect(subject.attachments.first.filename).to eq("book.mobi")
+    expect(subject.attachments.first.body.raw_source).to eq(book_mobi_binary_file)
+  end
 end
