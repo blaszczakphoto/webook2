45baa7a23236da5d217052825efe2f6063d18baa
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-07-26 21:14:45 +0200

    refactor operation

--- app/controllers/send_to_kindle_controller.rb
+++ app/controllers/send_to_kindle_controller.rb
@@ -2,7 +2,13 @@ class SendToKindleController < ApplicationController
   before_action :require_login
   
   def send_email
+    send_to_kindle_operation
+    redirect_to(webooks_index_path)
+  end
+
+  private
+
+  def send_to_kindle_operation
     SendToKindle::Send.new(kindle_email: current_user.kindle_email, book_id: params[:book_id]).call
-    redirect_to webooks_index_path
   end
 end
