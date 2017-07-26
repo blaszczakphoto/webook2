4876a374031d77ea3f6d7529b9c102bd11b171b2
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-07-26 16:44:33 +0200

    add test for controller send to kindle. next step

--- app/controllers/send_to_kindle_controller.rb
+++ app/controllers/send_to_kindle_controller.rb
@@ -2,6 +2,7 @@ class SendToKindleController < ApplicationController
   before_action :require_login
   
   def send_email
+    SendToKindle::Send.new(kindle_email: current_user.kindle_email, book_id: params[:book_id]).call
     redirect_to webooks_index_path
   end
 end
--- spec/controllers/send_to_kindle_controller_spec.rb
+++ spec/controllers/send_to_kindle_controller_spec.rb
@@ -12,12 +12,16 @@ RSpec.describe SendToKindleController, type: :controller do
     subject { get :send_email, params }
 
     it "initialize operation with valid params" do
+      send_operation_object = double("SendToKindle::Send").as_null_object
+      expect(SendToKindle::Send)
+        .to receive(:new)
+        .with(kindle_email: "john.doe@kindle.com", book_id: "2") { send_operation_object }
       get :send_email, params
-      send_operation_object = double("SendToKindle::Send")
-      expect(SendToKindle::Send).to receive(:new).with(kindle_email: "john.doe@kindle.com", book_id: "2")
     end
 
     it "redirect to webooks index" do
+      send_operation_object = double("SendToKindle::Send").as_null_object
+      allow_any_instance_of(SendToKindle::Send).to receive(:call)
       expect(subject).to redirect_to(webooks_index_path)
     end
   end
